a39712019d846bbdeaf3e90b52094ac8b872d136
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2017-10-19 18:53:17 -0200

    React Router + Redux Form: The Field Component - Unfinished

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,9 @@
 import _                      from 'lodash';
 import React, { Component }   from 'react';
 import { connect }            from 'react-redux';
+import { Link }               from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { fetchPosts }         from './../actions';
-
 
 class PostsIndex extends Component {
   componentDidMount() {
@@ -19,6 +19,12 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a post
+          </Link>
+        </div>
+
         <h3>Posts</h3>
         <ul className='list-group'>
           { this.renderPosts() }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import fetchPosts from './reducer_posts';
 
+
 const rootReducer = combineReducers({
+  form: formReducer,
   posts: fetchPosts
 });
 
